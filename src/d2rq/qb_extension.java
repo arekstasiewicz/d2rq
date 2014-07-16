30505f9a23469c8c95ed1b59fa5e1d37890d4691
Arkadiusz Stasiewicz <arekstasiewicz@gmail.com>
2014-07-16 11:49:38 +0100

    qb extension cleaning up

--- src/d2rq/qb_extension.java
+++ src/d2rq/qb_extension.java
@@ -6,6 +6,16 @@ import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.PrintStream;
 import java.io.UnsupportedEncodingException;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
 
 import jena.cmdline.ArgDecl;
 import jena.cmdline.CommandLine;
@@ -15,37 +25,49 @@ import org.apache.commons.logging.LogFactory;
 import org.d2rq.CommandLineTool;
 import org.d2rq.CompiledMapping;
 import org.d2rq.D2RQException;
+import org.d2rq.ResourceCollection;
 import org.d2rq.SystemLoader;
 import org.d2rq.db.SQLConnection;
 import org.d2rq.lang.D2RQReader;
 import org.d2rq.mapgen.MappingGenerator;
 import org.d2rq.mapgen.OntologyTarget;
+import org.d2rq.r2rml.qb.R2RMLQBParser;
+import org.d2rq.r2rml.qb.R2RMLQBWriter;
 
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.RDFWriter;
 import com.hp.hpl.jena.shared.NoWriterForLangException;
 
-
 /**
  * Command line interface for Data Cube mapping generator
  * 
+ * @author Richard Cyganiak (richard@cyganiak.de)
+ * @author Arkadiusz Stasiewicz (arekstasiewicz@gmail.com)
  */
 public class qb_extension extends CommandLineTool {
 	private final static Log log = LogFactory.getLog(qb_extension.class);
-	
+
+	private final static int DUMP_DEFAULT_FETCH_SIZE = 500;
+
+	private static R2RMLQBParser parser;
+
 	public static void main(String[] args) {
 		new qb_extension().process(args);
 	}
-	
+
 	public void usage() {
 		System.err.println("usage:");
-		System.err.println("  qb-extension [output-options] [connection-options] jdbcURL mappingFile");
-		System.err.println("  qb-extension [output-options] [connection-options] -l script.sql mappingFile");
+		System.err.println("  qb-extension [output-options] mappingFile");
 		System.err.println();
-		printStandardArguments(true, false);
+		System.err.println("  Arguments:");
+		System.err
+				.println("    mappingFile     Filename or URL of an extended R2RML mapping file");
 		System.err.println("  Options:");
-		printConnectionOptions(true);
-		System.err.println("    -o outfile.ttl  Output file name (default: stdout)");
+		System.err
+				.println("    -b baseURI      Base URI for RDF output (default: "
+						+ SystemLoader.DEFAULT_BASE_URI + ")");
+		System.err
+				.println("    -o outfile.ttl  Output file name (default: stdout)");
 		System.err.println("    --verbose       Print debug information");
 		System.err.println();
 		System.exit(1);
@@ -53,22 +75,26 @@ public class qb_extension extends CommandLineTool {
 	}
 
 	private ArgDecl baseArg = new ArgDecl(true, "b", "base");
-	private ArgDecl formatArg = new ArgDecl(true, "f", "format");
 	private ArgDecl outfileArg = new ArgDecl(true, "o", "out", "outfile");
-	
+
 	public void initArgs(CommandLine cmd) {
 		cmd.add(baseArg);
-		cmd.add(formatArg);
 		cmd.add(outfileArg);
 
-		setMinMaxArguments(1, 2);
+		setMinMaxArguments(1, 1);
 
 	}
 
+	/*
+	 *  1. load & parse extended-mapping
+	 *  2. generate DSD 
+	 *  3. generate proper R2RML mapping 
+	 *  4. output
+	 */
 	public void run(CommandLine cmd, SystemLoader loader) throws IOException {
 
-		String mappingFile;	
-		
+		String mappingFile;
+
 		if (cmd.numItems() == 1) {
 			mappingFile = cmd.getItem(0);
 		} else {
@@ -76,15 +102,8 @@ public class qb_extension extends CommandLineTool {
 			mappingFile = cmd.getItem(1);
 		}
 
-		loader.setMappingFile(mappingFile);
-		
 		if (cmd.hasArg(baseArg)) {
 			loader.setSystemBaseURI(cmd.getArg(baseArg).getValue());
-		}
-
-		String format = "N-TRIPLE";
-		if (cmd.hasArg(formatArg)) {
-			format = cmd.getArg(formatArg).getValue();
 		}
 
 		PrintStream out;
@@ -92,14 +111,17 @@ public class qb_extension extends CommandLineTool {
 			File f = new File(cmd.getArg(outfileArg).getValue());
 			log.info("Writing to " + f);
 			out = new PrintStream(new FileOutputStream(f));
-			loader.setSystemBaseURI(D2RQReader.absolutizeURI(f.toURI().toString() + "#"));
+			loader.setSystemBaseURI(D2RQReader.absolutizeURI(f.toURI()
+					.toString() + "#"));
 		} else {
 			log.info("Writing to stdout");
 			out = System.out;
 		}
 
- 		CompiledMapping mapping = loader.getMapping();
- 		
+		
+		R2RMLQBParser parser = new R2RMLQBParser(mappingFile);
+		out.println( parser.generateMapping() );
 
 	}
+
 }
