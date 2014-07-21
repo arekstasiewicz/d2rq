594d8391f0c348617ecef466af51411473d7b296
Arkadiusz Stasiewicz <arekstasiewicz@gmail.com>
2014-07-21 12:07:10 +0100

    cube parser - demo

--- src/org/d2rq/r2rml/qb/R2RMLQBParser.java
+++ src/org/d2rq/r2rml/qb/R2RMLQBParser.java
@@ -2,7 +2,6 @@ package org.d2rq.r2rml.qb;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,17 +11,12 @@ import javax.xml.parsers.ParserConfigurationException;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.d2rq.mapgen.MappingGenerator;
-import org.d2rq.mapgen.OntologyTarget;
-import org.d2rq.r2rml.R2RMLReader;
 import org.w3c.dom.Document;
-import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 import com.hp.hpl.jena.graph.query.Mapping;
-import com.hp.hpl.jena.rdf.model.Model;
 
 /**
  * Creates an R2RML {@link Mapping} from a input settings
@@ -33,6 +27,7 @@ public class R2RMLQBParser {
 
 	private final static String DEFAULT_DIMENSIONS = "dimension";
 	private final static String DEFAULT_MEASURES = "measure";
+	private final static String DEFAULT_CUBE = "dataset";
 
 	private List<Dimension> dimensions = null;
 	private List<Measure> measures = null;
@@ -43,14 +38,15 @@ public class R2RMLQBParser {
 	public R2RMLQBParser(String mappingFile) {
 
 		try {
-
 			Document doc = loadMapping(mappingFile);
+			
 			setCube(parseCube(doc));
 			setDimensions(parseDimensions(doc));
 			setMeasures(parseMeasures(doc));
 
 		} catch (org.xml.sax.SAXParseException e) {
 			System.err.println("Mapping file is not a valid XML file.");
+			System.exit(0);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -58,8 +54,19 @@ public class R2RMLQBParser {
 	}
 
 	private Cube parseCube(Document doc) {
-		// TODO Auto-generated method stub
-		return null;
+
+		Cube cube = null;
+
+		try {
+			Node nCube = doc.getDocumentElement()
+					.getElementsByTagName(DEFAULT_CUBE).item(0);
+			cube = new Cube(nCube);
+		} catch (NullPointerException e) {
+			System.err.println("Dataset configuration is missing.");
+			System.exit(1);
+		}
+
+		return cube;
 	}
 
 	private List<Dimension> parseDimensions(Document doc) {
@@ -74,10 +81,6 @@ public class R2RMLQBParser {
 			dimensions.add(new Dimension(temp, nNode));
 		}
 
-//		System.out.println("----------------------------");
-//		System.out.println(dimensions);
-//		System.out.println("----------------------------");
-
 		return dimensions;
 	}
 
@@ -85,17 +88,13 @@ public class R2RMLQBParser {
 
 		List<Measure> measures = new ArrayList<Measure>();
 
-		NodeList nDimensions = doc.getDocumentElement().getElementsByTagName(
+		NodeList nMeasures = doc.getDocumentElement().getElementsByTagName(
 				DEFAULT_MEASURES);
 
-		for (int temp = 0; temp < nDimensions.getLength(); temp++) {
-			Node nNode = nDimensions.item(temp);
-			dimensions.add(new Dimension(temp, nNode));
+		for (int temp = 0; temp < nMeasures.getLength(); temp++) {
+			Node nNode = nMeasures.item(temp);
+			measures.add(new Measure(temp, nNode));
 		}
-
-//		System.out.println("----------------------------");
-//		System.out.println(measures);
-//		System.out.println("----------------------------");
 
 		return measures;
 	}
@@ -117,10 +116,15 @@ public class R2RMLQBParser {
 		StringBuilder result = new StringBuilder();
 
 		result.append( generatePrefixList() );
-		result.append("# # # # # # # # # # #" + NEW_LINE);
-		result.append( generateDimensionsList() );
-		generateDimensionsList();
-		result.append("# # # # # # # # # # #" + NEW_LINE);
+		result.append("# ^-- prefixes # # # # # # # # # #" + NEW_LINE);
+		result.append( generateObservations() + NEW_LINE);
+		result.append("# ^-- observations # # # # # # # # # #" + NEW_LINE);		
+		result.append( generateDimensionsList() + NEW_LINE);
+		result.append("# ^-- dimensions # # # # # # # # # #" + NEW_LINE);
+		result.append( generateMeasuresList() + NEW_LINE);
+		result.append("# ^-- measures # # # # # # # # # #" + NEW_LINE);
+		result.append( generateDataStructure() + NEW_LINE);
+		result.append("# ^-- data structure # # # # # # # # # #" + NEW_LINE);
 
 		return result.toString();
 	}
@@ -131,43 +135,142 @@ public class R2RMLQBParser {
 		StringBuilder result = new StringBuilder();
 
 		result.append("@prefix map: <#>." + NEW_LINE);
-//		result.append("@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>." + NEW_LINE);
-//		result.append("@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> ." + NEW_LINE);
-//		result.append("@prefix rr: <http://www.w3.org/ns/r2rml#>." + NEW_LINE);
-//		result.append("@prefix xsd: <http://www.w3.org/2001/XMLSchema#>." + NEW_LINE);
-//		result.append("@prefix qb: <http://purl.org/linked-data/cube#> ." + NEW_LINE);
-//		result.append("@prefix sdmx-concept: <http://purl.org/linked-data/sdmx/2009/concept#> ." + NEW_LINE);
-//		result.append("@prefix sdmx-dimension: <http://purl.org/linked-data/sdmx/2009/dimension#> ." + NEW_LINE);
-//		result.append("@prefix sdmx-attribute: <http://purl.org/linked-data/sdmx/2009/attribute#> ." + NEW_LINE);
-//		result.append("@prefix sdmx-measure: <http://purl.org/linked-data/sdmx/2009/measure#> ." + NEW_LINE);
-//		result.append("@prefix sdmx-metadata: <http://purl.org/linked-data/sdmx/2009/metadata#> ." + NEW_LINE);
-//		result.append("@prefix sdmx-code: <http://purl.org/linked-data/sdmx/2009/code#> ." + NEW_LINE);
-//		result.append("@prefix sdmx-subject: <http://purl.org/linked-data/sdmx/2009/subject#> ." + NEW_LINE);
-//		result.append("@prefix skos: <http://www.w3.org/2004/02/skos/core#> ." + NEW_LINE);
+		result.append("@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>." + NEW_LINE);
+		result.append("@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> ." + NEW_LINE);
+		result.append("@prefix rr: <http://www.w3.org/ns/r2rml#>." + NEW_LINE);
+		result.append("@prefix xsd: <http://www.w3.org/2001/XMLSchema#>." + NEW_LINE);
+		result.append("@prefix qb: <http://purl.org/linked-data/cube#> ." + NEW_LINE);
+		result.append("@prefix sdmx-concept: <http://purl.org/linked-data/sdmx/2009/concept#> ." + NEW_LINE);
+		result.append("@prefix sdmx-dimension: <http://purl.org/linked-data/sdmx/2009/dimension#> ." + NEW_LINE);
+		result.append("@prefix sdmx-attribute: <http://purl.org/linked-data/sdmx/2009/attribute#> ." + NEW_LINE);
+		result.append("@prefix sdmx-measure: <http://purl.org/linked-data/sdmx/2009/measure#> ." + NEW_LINE);
+		result.append("@prefix sdmx-metadata: <http://purl.org/linked-data/sdmx/2009/metadata#> ." + NEW_LINE);
+		result.append("@prefix sdmx-code: <http://purl.org/linked-data/sdmx/2009/code#> ." + NEW_LINE);
+		result.append("@prefix sdmx-subject: <http://purl.org/linked-data/sdmx/2009/subject#> ." + NEW_LINE);
+		result.append("@prefix skos: <http://www.w3.org/2004/02/skos/core#> ." + NEW_LINE);
 		result.append(NEW_LINE);
 		
 		return result.toString();
 	}
 
 	private String generateDimensionsList() {
-		
 		StringBuilder result = new StringBuilder();
-		
+
 		for (Dimension dim : getDimensions()) {
-			
-			
-			result.append ( dim.getAsMapping() );
-			
-			
-			
+
+			result.append(dim.getAsMapping(getCube()));
+
 		}
-		return "debug dim: " + getDimensions().size() ;
+		return result.toString();
 	}
 
-	
-	
-	
-	
+	private String generateMeasuresList() {
+		StringBuilder result = new StringBuilder();
+
+		for (Measure m : getMeasures()) {
+			result.append(m.getAsMapping(getCube()));
+		}
+		return result.toString();
+	}
+
+	private String generateObservations() {
+
+		StringBuilder result = new StringBuilder();
+		
+		result.append("map:OBSERVATIONS" + NEW_LINE);
+		
+		result.append(NEW_LINE);
+
+		result.append("  rr:logicalTable [ rr:tableName '\"" + getCube().getTable() + "\"'; ];" + NEW_LINE);
+
+		result.append(NEW_LINE);
+
+		result.append("  rr:subjectMap [" + NEW_LINE);
+		result.append("    rr:template 'dataset/" + getCube().getUri() + "/" + getCube().getPattern() + "'; " + NEW_LINE);
+		result.append("    rr:class qb:Observation ;" + NEW_LINE);
+		result.append("  ];" + NEW_LINE);
+
+		result.append(NEW_LINE);
+
+		result.append("  rr:predicateObjectMap [" + NEW_LINE);
+		result.append("    rr:predicate qb:dataSet;" + NEW_LINE);
+		result.append("    rr:object <dataset/" + getCube().getUri() +"> ;" + NEW_LINE);
+		result.append("  ];" + NEW_LINE);
+
+		result.append(NEW_LINE);		
+
+		result.append( generateDimensionsDataForObservations() );
+
+		result.append(NEW_LINE);		
+
+		result.append( generateMeasuresDataForObservations() );
+
+		result.append("." + NEW_LINE);
+
+		result.append(NEW_LINE);
+
+		return result.toString();
+
+	}
+
+	private String generateDimensionsDataForObservations() {
+		StringBuilder result = new StringBuilder();
+
+		for (Dimension d : getDimensions()) {
+			result.append(d.getForObservations());
+		}
+
+		return result.toString();
+	}
+
+	private String generateMeasuresDataForObservations() {
+		StringBuilder result = new StringBuilder();
+
+		for (Measure m : getMeasures()) {
+			result.append(m.getForObservations());
+		}
+
+		return result.toString();
+	}
+
+	private String generateDataStructure() {
+		// TODO
+		return "";
+	}
+
+	public static String generateSampleMapping(){
+		StringBuilder result = new StringBuilder();
+
+		result.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>" + NEW_LINE);
+		result.append("<mapping>" + NEW_LINE);
+		result.append("  <dataset>" + NEW_LINE);
+		result.append("    <table-name>DB</table-name>" + NEW_LINE);
+		result.append("    <label>Dataset Label</label>" + NEW_LINE);
+		result.append("    <uri>my-dataset-uri</uri>" + NEW_LINE);
+		result.append("    <pattern>{\"ID\"}</pattern>" + NEW_LINE);
+		result.append("  </dataset>" + NEW_LINE);
+		result.append("  <dimensions>" + NEW_LINE);
+		result.append("    <dimension>" + NEW_LINE);
+		result.append("      <column>col</column>" + NEW_LINE);
+		result.append("      <label>Dimension Label</label>" + NEW_LINE);
+		result.append("      <uri>property-uri</uri>" + NEW_LINE);
+		result.append("      <property>dimension-property</property>" + NEW_LINE);
+		result.append("    </dimension>" + NEW_LINE);
+		result.append("  </dimensions>" + NEW_LINE);
+		result.append("  <measures>" + NEW_LINE);
+		result.append("    <measure>" + NEW_LINE);
+		result.append("      <label>Measure Label</label>" + NEW_LINE);
+		result.append("      <column>col</column>" + NEW_LINE);
+		result.append("      <uri>measure-uri</uri>" + NEW_LINE);
+		result.append("      <property>measure-property</property>" + NEW_LINE);
+		result.append("      <datatype>xsd:int</datatype>" + NEW_LINE);
+		result.append("    </measure>" + NEW_LINE);
+		result.append("  </measure>" + NEW_LINE);
+		result.append("</mapping>" + NEW_LINE);
+
+		return result.toString();
+	}
+
 	public List<Dimension> getDimensions() {
 		return dimensions;
 	}
