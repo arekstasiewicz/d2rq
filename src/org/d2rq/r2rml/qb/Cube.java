517a53a46f6d47708397f49764f1321b299faff4
Arkadiusz Stasiewicz <arekstasiewicz@gmail.com>
2014-07-21 11:53:39 +0100

    Cube cleanup

--- src/org/d2rq/r2rml/qb/Cube.java
+++ src/org/d2rq/r2rml/qb/Cube.java
@@ -8,12 +8,13 @@ public class Cube {
 	private int id = 0;
 	private String label = null;
 	private String uri = null;
+	private String pattern = null;
 	private String table = null;
 	
 	private static final String DEFAULT_LABEL 	= "dataset-label";
 	private static final String DEFAULT_URI 	= "dataset-uri";
 	private static final String DEFAULT_TABLE 	= "DB";
-	
+	private static final String DEFAULT_PATTERN = "{\"ID\"}";
 	
 	@Override
 	public String toString() {
@@ -25,6 +26,7 @@ public class Cube {
 		result.append(" DB table: " + table + NEW_LINE);
 		result.append(" Label: " + label + NEW_LINE);
 		result.append(" URI: " + uri + NEW_LINE);
+		result.append(" pattern: " + pattern + NEW_LINE);
 		result.append("}");
 
 		return result.toString();
@@ -41,7 +43,8 @@ public class Cube {
 			String table;
 			String label;
 			String uri;
-			
+			String pattern;
+
 			try {
 				table = eElement.getElementsByTagName("table").item(0)
 						.getTextContent();
@@ -61,7 +64,7 @@ public class Cube {
 				System.err.println("Setting default value.");
 				label = DEFAULT_LABEL + "-" + getId();
 			}
-			
+
 			try {
 				uri = eElement.getElementsByTagName("uri").item(0)
 						.getTextContent();
@@ -72,10 +75,20 @@ public class Cube {
 				uri = DEFAULT_URI + "-" + getId();
 			}
 
+			try {
+				pattern = eElement.getElementsByTagName("pattern").item(0)
+						.getTextContent();
+			} catch (NullPointerException e) {
+				System.err.println("Cube " + id
+						+ " is not a valid, pattern is missing.");
+				System.err.println("Setting default value.");
+				pattern = DEFAULT_PATTERN + "-" + getId();
+			}
+
 			setTable(table);
 			setLabel(label);
 			setUri(uri);
-
+			setPattern(pattern);
 		}
 	}
 
@@ -118,9 +131,16 @@ public class Cube {
 		this.uri = uri;
 	}
 
+	public String getPattern() {
+		return pattern;
+	}
+
+	public void setPattern(String pattern) {
+		this.pattern = pattern;
+	}
 
 	public Boolean validate() {
-		// TODO Auto-generated method stub
+		// TODO
 		return null;
 	}
 }
